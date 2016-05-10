e3556e4d30be731c871e9a265557521479f9615e
lx9t01 <alex.xuhaixiang@gmail.com>
2016-05-09 22:12:51 -0700

    v0.4 cuh

--- Cuda1DFDWave_cuda.cuh
+++ Cuda1DFDWave_cuda.cuh
@@ -10,6 +10,10 @@
 /* TODO: This is a CUDA header file.
 If you have any functions in your .cu file that need to be
 accessed from the outside, declare them here */
-
+void cudaWaveKernel(const float* dev_old_data,
+    const float* dev_cur_data,
+    float* dev_new_data,
+    const size_t numberOfNodes,
+    const float courantSquared);
 
 #endif // CUDA_1D_FD_WAVE_CUDA_CUH
