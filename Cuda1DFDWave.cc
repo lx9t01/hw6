38512b9f0715dd38550f34b7606226dd27701a0f
lx9t01 <alex.xuhaixiang@gmail.com>
2016-05-09 22:24:55 -0700

    try

--- Cuda1DFDWave.cc
+++ Cuda1DFDWave.cc
@@ -198,8 +198,8 @@ int main(int argc, char* argv[]) {
         The right boundary conditon will be 0 at the last position
         for all times t */
         float right_boundary_value = 0.0;
-        cudaMemcpy(dev_new_data, &left_boundary_value, 1*sizeof(float), cudaMemcpyHostToDevice);
-        cudaMemcpy(dev_new_data + numberOfTimesteps, &right_boundary_value, 1*sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(dev_new_data, &left_boundary_value, 1 * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(dev_new_data + numberOfTimesteps, &right_boundary_value, 1 * sizeof(float), cudaMemcpyHostToDevice);
 
         float* temp = dev_old_data;
         dev_old_data = dev_cur_data;
