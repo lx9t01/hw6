73dcc287f084d5a61e90ac6df986956754dcffbe
lx9t01 <alex.xuhaixiang@gmail.com>
2016-05-14 22:32:55 -0700

    ;

--- Q2/gillespie.cu
+++ Q2/gillespie.cu
@@ -127,7 +127,7 @@ void cudaResampleKernel(
         int i = (int)(dev_accu_time[idx] * 10);
         // printf("inside resample kernel, idx: %d, i: %d\n", idx, i);
         for (int j = 0; j < i && j < T; ++j) {
-            printf("j: %d\n", j)
+            printf("j: %d\n", j);
             if (dev_resample_X[idx * T + j] < 0) {
                 dev_resample_X[idx * T + j] = 1.0;
                 printf("%d %d, %f\n",idx, j, dev_X[idx]);
