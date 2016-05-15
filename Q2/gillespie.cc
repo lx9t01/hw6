62f849cd67af66abeadbd35bf214f1e285c5ed91
lx9t01 <alex.xuhaixiang@gmail.com>
2016-05-15 02:07:09 -0700

    max

--- Q2/gillespie.cc
+++ Q2/gillespie.cc
@@ -161,6 +161,13 @@ int main (int argc, char** argv) {
         //     }
         // }
         // *host_min_time = new_min;
+        float new_max = -1;
+        for (int i = 0; i < N; ++i) {
+            if (test_accu[i] > new_min) {
+                new_min = test_accu[i];
+            }
+        }
+        printf("max get this max: %f \n", new_max);
 
         printf("min get ");
         printf("this min: %f\n", *host_min_time);
